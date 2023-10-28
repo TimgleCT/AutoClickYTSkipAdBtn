6167e7c0e47bdc4bef29e81d0aeda3028de5b8fb
TimgleCT <ct25283115@gmail.com>
2023-10-28 18:02:15 +0800

    refactor: 調整成物件導向寫法
    - 建立 YouTubeAdSkipper 類別

--- content.js
+++ content.js
@@ -1,60 +1,48 @@
-function clickSkipButton() {
-    // 檢查節點是否包含略過廣告按鈕
-    const skipButton = document.querySelector('.ytp-ad-skip-button.ytp-button');
-    if (skipButton) {
-        // 點擊略過廣告按鈕
-        skipButton.click(); 
-        console.log(new Date(), '自動點擊廣告');
-        return true;
+class YouTubeAdSkipper {
+    constructor() {
+        const config = { childList: true, subtree: true };
+        this.hasAddListener = false;
+        this.observer = new MutationObserver(this.handleMutations.bind(this));
+        this.observer.observe(document, config);
     }
-    return false;
-}
-
-var hasAddListener = false;
-function moveToLastSecond(video){
-    // 移至廣告片尾
-    console.log(new Date(), '略過不可點擊廣告');
-    video.currentTime = video.duration;
-    hasAddListener = true;
-}
-function endAd(video){
-    video.removeEventListener('canplay', moveToLastSecond);
-    hasAddListener = false;
-    video.removeEventListener('ended', endAd);
-}
-
-
-function skipAdWithoutBtn() {
-    const checkAdWithoutBtn = document.querySelector('.ytp-ad-player-overlay');
-    if (checkAdWithoutBtn) {
-        if (!hasAddListener) {
+    handleMutations(mutationsList) {
+        for (const mutation of mutationsList) {
+            if (mutation.addedNodes.length > 0) {
+                const hasSkipBtn = this.clickSkipButton();
+                if (hasSkipBtn){
+                    break;
+                }
+                const hasAdWithoutBtn = this.skipAdWithoutBtn();
+                if (hasAdWithoutBtn){
+                    break;
+                }
+            }
+        }
+    }
+    clickSkipButton(){
+        const skipButton = document.querySelector('.ytp-ad-skip-button.ytp-button');
+        if (skipButton) {
+            skipButton.click(); 
+            console.log(new Date(), '自動點擊廣告');
+            return true;
+        }
+        return false;
+    }
+    skipAdWithoutBtn(){
+        const checkAdWithoutBtn = document.querySelector('.ytp-ad-player-overlay');
+        if (checkAdWithoutBtn && !this.hasAddListener) {
             const video = document.querySelector('video');
-            video.addEventListener('canplay', moveToLastSecond(video));
-            video.addEventListener("ended", endAd(video));
+            video.addEventListener('canplay', this.moveToLastSecond(video), { once: true });
+            video.addEventListener('ended', () => { this.hasAddListener = false; }, { once: true });
+            return true;
         }
-        return true;
+        return false;
     }
-    return false;
+    moveToLastSecond(video){
+        console.log(new Date(), '略過不可點擊廣告');
+        video.currentTime = video.duration;
+        this.hasAddListener = true;
+    }
 }
 
-// 使用Mutation Observer來監視DOM變化
-const observer = new MutationObserver(mutationsList => {
-    for (const mutation of mutationsList) {
-        if (mutation.addedNodes.length > 0) {
-            const hasSkipBtn = clickSkipButton();
-            if (hasSkipBtn){
-                break;
-            }
-            const hasAdWithoutBtn = skipAdWithoutBtn();
-            if (hasAdWithoutBtn){
-                break;
-            }
-        }
-    }
-});
-
-// 設定Mutation Observer的配置
-const config = { childList: true, subtree: true };
-
-// 監視整個文檔的變化
-observer.observe(document, config);
+const adSkipper = new YouTubeAdSkipper();
